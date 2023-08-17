77868fbc90eb2250c5e56378532598a5494ecb3f
MasterHongYu <mhongyu951023@gmail.com>
2023-08-17 20:49:39 +0800

    v2.0.1

--- guess_number.py
+++ guess_number.py
@@ -1,44 +1,5 @@
 def guess_number_game ():
 
-    def answer(n):
-        from random import choice as c
-        number,ans = [int(i) for i in range(10)],[]
-        for i in range(n) :
-            ans.append(c(number))
-            number.remove(ans[i])
-        return(ans)
-    
-    def guess(k,d) :
-        try :
-            guess = [int(i) for i in input(f"請輸入{d}位數字，並且每個數字間都要有空格，不可輸入重複數字，你還有{(2 * d) + 2 - k}次機會：").split()]
-            if len(guess) == d and len(set(guess)) == d and (0 <= i < 10 for i in guess) :
-                return(guess)
-            else : 
-                errorkey = Error()
-                return errorkey
-        except :
-            errorkey = Error()
-            return errorkey
-
-    def Error ():
-        print(f"輸入格式錯誤，或是輸入位數不正確，請再輸入一次。")
-        return "?"
-    
-    def AnBn(playerGuess,ans,l):
-        An,Bn = 0,0
-        for i in range(l):
-            if playerGuess[i] == ans[i] : 
-                An += 1
-            elif playerGuess[i] in ans : 
-                Bn += 1 
-        print("%d A %d B"%(An,Bn))
-        if An == l : 
-            return "win"
-        
-    def play_again():
-        if input("要再玩一次嗎？如果要的話請輸入\"again\"，不用的話，請輸入除了\"again\"以外的值：") == "again" :
-            return "again"
-    
     def digi():
         while True :
             try : 
@@ -51,27 +12,68 @@ def guess_number_game ():
             Error()
         return digital
 
+    def answer(n):
+        from random import choice as c
+        number,ans = [int(i) for i in range(10)],[]
+        for i in range(n) :
+            ans.append(c(number))
+            number.remove(ans[i])
+        return(ans)
+    
+    def guess(times_guess,digi_guess) :
+        try :
+            guess = [int(i) for i in input(f"請輸入{digi_guess}位數字，並且每個數字間都要有空格，不可輸入重複數字，你還有{(2 * digi_guess) + 2 - times_guess}次機會：").split()]
+            if len(guess) == digi_guess and len(set(guess)) == digi_guess and (0 <= i < 10 for i in guess) :
+                return(guess)
+            else : 
+                errorkey = Error()
+                return errorkey
+        except :
+            errorkey = Error()
+            return errorkey
+
+    def AnBn(playerGuess,ans,digi_AnBn,times_AnBn):
+        An,Bn = 0,0
+        for i in range(digi_AnBn):
+            if playerGuess[i] == ans[i] : 
+                An += 1
+            elif playerGuess[i] in ans : 
+                Bn += 1 
+        print("%d A %d B"%(An,Bn))
+        if An == digi_AnBn :
+            print(f"你贏了!總共花了{times_AnBn}次。") 
+            return "win"
+    
+    def Error ():
+        print(f"輸入格式錯誤，或是輸入位數不正確，請再輸入一次，謝謝。")
+        return "?"
+        
+    def play_again():
+        if input("要再玩一次嗎？如果要的話請輸入\"again\"，不用的話，請輸入除了\"again\"以外的值：") == "again" :
+            return "again"
+    
     def main():
         print("1A2B Game")
-        n = digi() 
-        ans,times = answer(n),0
+        d = digi() 
+        ans,times = answer(d),0
         #print(ans)
-        while times < (2 * n + 2):
-            check = guess(times,n)
+        while times < (2 * d + 2):
+            check = guess(times,d)
             if check == "?" : 
                 continue
             times += 1
-            if (res := AnBn(check,ans,n))  == "win" :
-                print(f"你贏了!總共花了{times}次。")
+            if (res := AnBn(check,ans,d,times))  == "win" :
                 break
         else : 
-            print(f"遊戲結束，你輸了，正確號碼為{ans[0]}{ans[1]}{ans[2]}{ans[3]}，太可惜了。")
+            print(f"遊戲結束，你輸了，正確號碼為{''.join([str(i) for i in ans])}，太可惜了。")
 
         if (again := play_again()) == "again" : 
             main()
-
+        else :
+            print("Bye:>")
+   
     main()
-    print("bye")
+
 guess_number_game()
     
 
