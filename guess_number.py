1dca744bb98bf465983e671154de19a65706ab63
MasterHongYu <mhongyu951023@gmail.com>
2023-08-17 20:59:58 +0800

    v2.0.2

--- guess_number.py
+++ guess_number.py
@@ -1,5 +1,6 @@
 def guess_number_game ():
 
+    # 確認位數的函數
     def digi():
         while True :
             try : 
@@ -11,7 +12,7 @@ def guess_number_game ():
                 break
             Error()
         return digital
-
+    # 生成答案的函數
     def answer(n):
         from random import choice as c
         number,ans = [int(i) for i in range(10)],[]
@@ -19,7 +20,7 @@ def guess_number_game ():
             ans.append(c(number))
             number.remove(ans[i])
         return(ans)
-    
+    # 確認玩家輸入的答案格式，是否正確的函數
     def guess(times_guess,digi_guess) :
         try :
             guess = [int(i) for i in input(f"請輸入{digi_guess}位數字，並且每個數字間都要有空格，不可輸入重複數字，你還有{(2 * digi_guess) + 2 - times_guess}次機會：").split()]
@@ -31,7 +32,7 @@ def guess_number_game ():
         except :
             errorkey = Error()
             return errorkey
-
+    # 執行1A2B的運算
     def AnBn(playerGuess,ans,digi_AnBn,times_AnBn):
         An,Bn = 0,0
         for i in range(digi_AnBn):
@@ -43,15 +44,15 @@ def guess_number_game ():
         if An == digi_AnBn :
             print(f"你贏了!總共花了{times_AnBn}次。") 
             return "win"
-    
+    # 錯誤函數
     def Error ():
         print(f"輸入格式錯誤，或是輸入位數不正確，請再輸入一次，謝謝。")
         return "?"
-        
+    # 確認玩家是否要再玩一輪的函數
     def play_again():
         if input("要再玩一次嗎？如果要的話請輸入\"again\"，不用的話，請輸入除了\"again\"以外的值：") == "again" :
             return "again"
-    
+    # 運行遊戲流程的函數
     def main():
         print("1A2B Game")
         d = digi() 
