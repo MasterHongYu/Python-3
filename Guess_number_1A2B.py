c1022ceb8397960c6baed5ba87549a957f89a807
MasterHongYu <mhongyu951023@gmail.com>
2023-08-18 13:12:40 +0800

    v 1.1

--- Guess_number_1A2B.py
+++ Guess_number_1A2B.py
@@ -22,10 +22,16 @@ def guess_number_game ():
         return(ans)
     # 確認玩家輸入的答案格式，是否正確的函數
     def guess(times_guess,digi_guess) :
+        input_guess = input(f"請輸入{digi_guess}位數字，你還有{(2 * digi_guess) + 2 - times_guess}次機會：")
         try :
-            guess = [int(i) for i in input(f"請輸入{digi_guess}位數字，並且每個數字間都要有空格，不可輸入重複數字，你還有{(2 * digi_guess) + 2 - times_guess}次機會：").split()]
+            guess = [int(i) for i in input_guess.split()]
             if len(guess) == digi_guess and len(set(guess)) == digi_guess and (0 <= i < 10 for i in guess) :
-                return(guess)
+                return guess
+            elif len(guess) == 1 and len(set(list(input_guess))) == digi_guess:
+                guess = list(input_guess)
+                for i in range(digi_guess) :
+                    guess[i] = int(guess[i])
+                return guess
             else : 
                 errorkey = Error()
                 return errorkey
@@ -56,6 +62,7 @@ def guess_number_game ():
     def main():
         print("1A2B Game")
         d = digi() 
+        print("遊戲規則：\n一、輸入的數字格式為2種：玩家所輸入的全部數字之間，均有空格，或均無空格。\n二、不可輸入重複數字。\n若無遵照規則指示，會跳出錯誤警示。")
         ans,times = answer(d),0
         #print(ans)
         while times < (2 * d + 2):
