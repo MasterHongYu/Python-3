1fb3352de8c55d2381f5ef2c4f9461415b378780
MasterHongYu <mhongyu951023@gmail.com>
2023-08-18 16:00:50 +0800

    v 1.0

--- Guess_number_100.py
+++ Guess_number_100.py
@@ -3,39 +3,48 @@ def Guess_number_100 ():
     def anwser() :
         import random as ran 
         ans = ran.randint(1,99)
-        print(ans)
+        #print(ans)
         return ans
     
     def contract(player_ans,min,max,ans) :
         if 1 <= player_ans <= 99 :
             if player_ans == ans :
-                print('Youwin!')
-                return -1,-1
+                cheak = 2
+            if player_ans < min or player_ans > max :
+                Error()
+                cheak = 1
             if player_ans > min and player_ans < ans:
-                min = player_ans
+                min,cheak = player_ans,0
             if player_ans < max and player_ans > ans :
-                max = player_ans
+                max,cheak = player_ans,0
+            return min,max,cheak
         else :
             Error()
-            return -2,-2
+            cheak = 1
+            return min,max,cheak
 
     def Error():
-        print("輸入錯誤")
+        print("輸入格式錯誤")
         
     def main():
-        ans,min,max = anwser(),1,99 
-        while True :
+        ans,min,max,times= anwser(),1,99,0
+        while times < 8 :
+            cheak = 0
             try :
-                n = int(input("請輸入1~99範圍內的數字"))
-                min,max = contract(n,min,max,ans)
-                if min == max == -2 :
+                n = int(input(f"請輸入{min}~{max}範圍內的數字，你還有{8-times}次機會："))
+                min,max,cheak = contract(n,min,max,ans)
+                if cheak == 1 :
                     continue
-                if min == max == -1 :
-                    print(f"Youwin,{ans}")
+                if cheak == 2 :
+                    times += 1
+                    print(f"你贏了，總共花了{times}次")
                     break
-                print(f"{min} < 答案 < {max}")
+                print(f"{min} ≤ 答案 ≤ {max}")
+                times += 1
             except :
                 Error()
+        else :
+            print(f"你輸了，答案是{ans}")
     main()
 Guess_number_100 ()
     
