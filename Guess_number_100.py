93523e7c16cc4f97950d91f968173b1850928c1a
MasterHongYu <mhongyu951023@gmail.com>
2023-08-20 18:37:32 +0800

    v 1.1.4 簡化程式碼

--- Guess_number_100.py
+++ Guess_number_100.py
@@ -8,12 +8,9 @@ def Guess_number_100 ():
                 digit = int(input("你想猜的數字範圍要是幾位數?(最少2位數，最多6位數)："))
                 if 2 <= digit <= 6 :
                     return digit
-                Error()
             except :
-                Error()
-    # 生成答案的函數
-    def anwser(d) :
-        return random.randint(1,pow(10,d) - 1)
+                None
+            Error()
     # 執行大小比較運算的函數
     def contract(min,max,ans,player_ans,digi_contract) :
         if 1 <= player_ans <= pow(10,digi_contract) - 1 :
@@ -27,24 +24,19 @@ def Guess_number_100 ():
                 min = player_ans
             if player_ans < max and player_ans > ans :
                 max = player_ans
-            return min,max,cheak
         else :
             Error()
             cheak = 1
-            return min,max,cheak
+        return min,max,cheak
     # 錯誤指示的函數
     def Error():
         print(f"輸入格式錯誤，或是輸入位數不正確，請再輸入一次，謝謝。\n")
-    # 確認玩家是否要再玩一回合的函數
-    def play_again():
-        if input("要再玩一次嗎？如果要的話請輸入\"again\"，不用的話，請輸入除了\"again\"以外的值：\n") == "again" :
-            return "again"
     # 運行遊戲流程的函數
     def main():
         print("100 game")
-        d = digi()
+        d,times = digi(),0
         print(f"\n遊戲規則：\n一、輸入的數字格式為連續的數字[例：10、123]\n二、請輸入題目範圍內的數字。\n三、若無遵照規則指示操作，則會跳出錯誤警示，不過程式碼不會崩潰，請放心。\n四、猜測總次數為{4 * d - 2}次。\n五、如果你想放棄本回合，請輸入\"F\"。\n")
-        ans,min,max,times= anwser(d = d),1,pow(10,d) - 1,0
+        ans,min,max= random.randint(1,pow(10,d) - 1),1,pow(10,d) - 1
         while times < 4 * d - 2 :
             n = (input(f"請輸入{min}~{max}範圍內的數字，你還有{4 * d - 2 - times}次機會："))
             try :
@@ -52,13 +44,12 @@ def Guess_number_100 ():
                 min,max,cheak = contract(min = min, max = max, ans = ans, player_ans = n, digi_contract = d)
                 if cheak == 1 :
                     continue
+                times += 1
                 if cheak == 2 :
-                    times += 1
                     print(f"你贏了，總共花了{times}次")
                     break
                 if times < 4 * d - 2 :
                     print(f"{min} < 答案 < {max}\n")
-                times += 1
             except :
                 if n == "F" :
                     print("您已放棄本回合的遊戲。")
@@ -66,15 +57,12 @@ def Guess_number_100 ():
                 Error()
         else :
             print(f"你輸了，答案是{ans}")
-
-        if (again := play_again()) == "again" : 
+        if input("要再玩一次嗎？如果要的話請輸入\"again\"，不用的話，請輸入除了\"again\"以外的值：\n") == "again" : 
             return "again"
-        else :
-            print("Bye:>")
+        print("Bye:>")
     
-    while (a := main()) == "again" :
+    while main() == "again" :
         print()
-        continue
 
 Guess_number_100 ()
     
