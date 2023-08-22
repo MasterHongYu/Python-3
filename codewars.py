6ebad4510d31d5031acc599ab4bac07a32baaf50
MasterHongYu <mhongyu951023@gmail.com>
2023-08-23 00:19:52 +0800

    _

--- codewars.py
+++ codewars.py
@@ -1,10 +1,6 @@
 def sum_edges(n): 
-    Sum,a = 0,5
-    for i in range(sum(range(1,n + 1)) + 1) : 
-        Sum += i
-    for i in range(1,n+1) :
-        if i >= 3 and i < n :
-            Sum -= sum(range(a,a+(i-2)))
-            a += i
+    Sum,a = sum(range(1,(sum(range(1,n + 1))+ 1))),5
+    for i in range(3,n) :Sum -= sum(range(a,a+(i-2)));a += i
     return(Sum)
-print(sum_edges(int(input())))
+for i in list(map(sum_edges,[int(i) for i in input().split()])) :
+    print(i)
